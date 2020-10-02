572bbe2949ac4f2741a35afc308b714a00008b56
iyus-anonim <rendrayusri.ry@gmail.com>
2020-10-02 15:24:29 +0700

    biodata

---
+++ biodata/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="style.css" type="text/css" rel="stylesheet">
+ <meta charset="UTF-8">
+ <title>biodata</title>
+</head>
+
+<body>
+    <div class="header">
+        <h1>Data Diri</h1>
+    </div>
+    <div class="sidebar">
+        <figure>
+            <img src="5170411021.jpg" width="200px" height="300px">
+            <figcaption>
+                <p2>Rendra Yusrimaelani</p2><br>
+                <p3>Junior Web Developer</p3>
+            </figcaption>
+        </figure>
+    </div>
+    <div class="content">
+        <div class="info">
+            <h1>
+                <b>Data diri<br><br>
+            </h1>
+                <p4> Umur     : 20 tahun <br>
+                <hr>
+                Nomor HP : +62-822-5131-6196<br>
+                <hr>
+                Email    : Rendrayusri.ry@gmail.com <br>
+                <hr>
+            </p4>
+        </div>
+        <div class="cerita">
+       <h1><b>Biografi</b> <br><br></h1>
+            <p4>
+                &nbsp; &nbsp;&nbsp; Saya lahir di sukamandang 23 juni 1999. Asal dari kalimantan tengah
+                dan sekarang saya sedang menempuh pendidikan di salah satu universitas swasta
+                di DI.Yogyakarta dengan jurusan Teknik Informatika semester 5. <br><br>
+                &nbsp; &nbsp;&nbsp;Alamat asal saya yaitu desa sukajaya RT 008/ RW/001 kecamatan seruyan tengah
+                kabupaten seruyan provinsi kalimatan tengah. dan alamat saya di jogja berada di dusun 
+                nganti RT/02/RW/07 sendang adi mlati sleman Yogyakarta.
+                mungkin itu sedikit biografi tentang saya. kurang lebihnya mohon maaf yang sebesar-besarnya. 
+        </p4>
+        </div>
+
+    </div>
+    <div class="footer">
+        <p1>kontak saya<br>
+            ig:@rendra.ym fb:rendra RYM
+         </p1>
+    </div>
+</body>
+</html>
